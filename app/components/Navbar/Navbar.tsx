13147edd84ba8c25787b5115b169d37f24f20eee
Kovercodes <sas.gta@ya.ru>
2023-12-10 23:23:31 +0500

    feat: search and hamburger in header

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 export default function Navbar() {
   return (
@@ -8,16 +10,12 @@ export default function Navbar() {
       <div className="py-4 border-b-[1px]">
         <Container>
           <div
-            className="
-              flex
-              flex-row
-              items-center
-              justify-between
-              gap-3
-              md:gap-0
+            className="flex flex-row items-center justify-between gap-3 md:gap-0
             "
           >
             <Logo />
+            <Search />
+            <UserMenu />
           </div>
         </Container>
       </div>
